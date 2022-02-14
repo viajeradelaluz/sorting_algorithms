c04d4ecea7d60c95c1e22f0e797a7b0d7d71366b
Ella711 <allenro711@gmail.com>
2022-02-14 10:43:41 -0500

    [FIX] - Betty styling

--- sort.h
+++ sort.h
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -26,3 +26,4 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
+
